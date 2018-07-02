0b414b3e329b58abf37dc011dba87a83110fcc3e
José Guillermo Saldaña <memo.sal98@gmail.com>
2018-07-02 18:33:53 -0500

    More Express exercises imported

---
+++ Exercises/Back End/MoreExpress/ejsdemo/app.js
@@ -0,0 +1,27 @@
+var express = require("express");
+var app = express();
+
+
+app.get("/", function(req,res) {
+  res.render("home.ejs");
+});
+
+app.get("/fallinlovewith/:thing", function(req,res){
+  var thing = req.params.thing;
+  res.render("love.ejs", {thingVar: thing  });
+})
+
+app.get("/posts", function(req , res) {
+  var posts = [
+    {title: "Post 1", author: "Susy"  },
+    {title: "My adorable pet bunny", author: "Charlie"},
+    {title: "Can you believe this pomsky?", author: "Colt"}
+  ];
+  res.render("posts.ejs", {posts:posts});
+});
+
+
+
+app.listen(process.env.PORT,process.env.IP,function() {
+  console.log("Server is listening!");
+});
