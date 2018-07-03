84e838448e4210b6a9a96e9c03100c758d3e391f
José Guillermo Saldaña <memo.sal98@gmail.com>
2018-07-03 11:34:16 -0500

    Added partials to dry up code

--- Exercises/Back End/MoreExpress/ejsdemo/app.js
+++ Exercises/Back End/MoreExpress/ejsdemo/app.js
@@ -1,14 +1,18 @@
 var express = require("express");
 var app = express();
 
+app.use(express.static("public"))
+app.set("view engine", "ejs");
+
+
 
 app.get("/", function(req,res) {
-  res.render("home.ejs");
+  res.render("home");
 });
 
 app.get("/fallinlovewith/:thing", function(req,res){
   var thing = req.params.thing;
-  res.render("love.ejs", {thingVar: thing  });
+  res.render("love", {thingVar: thing  });
 })
 
 app.get("/posts", function(req , res) {
@@ -17,7 +21,7 @@ app.get("/posts", function(req , res) {
     {title: "My adorable pet bunny", author: "Charlie"},
     {title: "Can you believe this pomsky?", author: "Colt"}
   ];
-  res.render("posts.ejs", {posts:posts});
+  res.render("posts", {posts:posts});
 });
 
 
