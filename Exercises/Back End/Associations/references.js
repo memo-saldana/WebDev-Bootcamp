117d0a180c60de14aaa5bde160614fd0f2025060
José Guillermo Saldaña <memo.sal98@gmail.com>
2018-07-25 10:10:08 -0500

    Refactored models as modules

--- Exercises/Back End/Associations/references.js
+++ Exercises/Back End/Associations/references.js
@@ -1,76 +1,57 @@
 var mongoose = require("mongoose");
 mongoose.connect("mongodb://localhost:27017/blog_demo", { useNewUrlParser: true });
 
-
-// POST - title, content
-var postSchema = new mongoose.Schema({
-	title: String,
-	content: String
-});
-
-var Post = mongoose.model("Post", postSchema);
-
-
-// USER - email, name
-var userSchema = new mongoose.Schema({
-	email: String,
-	name: String,
-	posts: [{
-		type: mongoose.Schema.Types.ObjectId,
-		ref: "Post"
-	}]
-});
-
-var User = mongoose.model("User", userSchema);
+var Post = require ("./models/post");
+var User = require("./models/user");
  
-User.remove({}, function() {
-    Post.remove({}, function() {
-        User.create({
-            email: "bob@gmail.com",
-            name: "Bob Belcher"
-        }, function(err, user) {
-            if(err) {
-                console.log(err);
-            } else {
-                User.findOne({email: "bob@gmail.com"}).populate("posts").exec(function(err, foundUser){
-                    if(err) {
-                        console.log(err);
-                    } else {
-                        console.log(foundUser);
-                        Post.create({
-                          title: "How to cook the best burger pt. 2",
-                          content: "blah blah blah blah blah"
-                        }, function(err, post){
-                            if(err) {
-                                console.log(err);
-                            } else {
-                                User.findOne({email: "bob@gmail.com"}, function(err, foundUser){
-                                    if(err){
-                                        console.log(err);
-                                    } else {
-                                        foundUser.posts.push(post._id);
-                                        foundUser.save(function(err, data){
-                                            if(err){
-                                                console.log(err);
-                                            } else {
-                                                console.log(data);
-                                            }
-                                        });
-                                    }
-                                });
-                            }
-                        });
-                    }
-                });
-            }
-        });
+// User.remove({}, function() {
+//     Post.remove({}, function() {
+//         User.create({
+//             email: "bob@gmail.com",
+//             name: "Bob Belcher"
+//         }, function(err, user) {
+//             if(err) {
+//                 console.log(err);
+//             } else {
+//                 User.findOne({email: "bob@gmail.com"}).populate("posts").exec(function(err, foundUser){
+//                     if(err) {
+//                         console.log(err);
+//                     } else {
+//                         console.log(foundUser);
+//                         Post.create({
+//                           title: "How to cook the best burger pt. 2",
+//                           content: "blah blah blah blah blah"
+//                         }, function(err, post){
+//                             if(err) {
+//                                 console.log(err);
+//                             } else {
+//                                 User.findOne({email: "bob@gmail.com"}, function(err, foundUser){
+//                                     if(err){
+//                                         console.log(err);
+//                                     } else {
+//                                         foundUser.posts.push(post._id);
+//                                         foundUser.save(function(err, data){
+//                                             if(err){
+//                                                 console.log(err);
+//                                             } else {
+//                                                 console.log(data);
+//                                             }
+//                                         });
+//                                     }
+//                                 });
+//                             }
+//                         });
+//                     }
+//                 });
+//             }
+//         });
  
-    });
-});
+//     });
+// });
 
 
 // Post.create({
-// 	title: "Burgers and more!",
+// 	title: "Burgers and moreso 4!",
 // 	content: " asdfasdfas asf"
 // },function(err,post) {
 // 	if(err){
