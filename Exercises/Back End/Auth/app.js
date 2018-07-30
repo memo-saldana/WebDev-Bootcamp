37417042d29f6fde6761ed506ec0819010838539
José Guillermo Saldaña <memo.sal98@gmail.com>
2018-07-30 18:36:59 -0500

    Finished Auth exercise

--- Exercises/Back End/Auth/app.js
+++ Exercises/Back End/Auth/app.js
@@ -9,6 +9,8 @@ var express = require("express"),
 mongoose.connect("mongodb://localhost:27017/auth_demo_app",{useNewUrlParser: true});
 
 var app = express();
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({extended: true}))
 
 app.use(require("express-session")({
 	secret: "Rusty is the best and cutest dog in the world",
@@ -18,18 +20,72 @@ app.use(require("express-session")({
 app.use(passport.initialize());
 app.use(passport.session());
 
+passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
-app.set("view engine", "ejs");
+// 
+// ROUTES 
+// 
 
 app.get("/", function(req,res) {
 	res.render("home");
 });
 
-app.get("/secret",function(req,res) {
+app.get("/secret",isLoggedIn,function(req,res) {
 	res.render("secret");
 })
+
+// 
+// AUTH ROUTES
+// 
+app.get("/register", function(req,res) {
+	res.render("register"); 
+});
+
+app.post("/register", function(req,res) {
+	User.register(new User({username: req.body.username}), req.body.password, function(err,user) {
+		if(err){
+			console.log(err);
+			res.render("register");
+		} else {
+			passport.authenticate("local")(req, res, function() { 
+				res.redirect("/secret");
+			});
+		}
+	})
+});
+
+// 
+// LOGIN ROUTES
+// 
+
+
+app.get("/login",function(req,res) {
+	res.render("login");
+});
+
+app.post("/login",passport.authenticate("local", {
+	successRedirect: "/secret",
+	failureRedirect: "/login"
+}), function(req,res) {
+});
+
+// 
+// LOGOUT ROUTES
+// 
+
+app.get("/logout",function(req,res) {
+	req.logout();
+	res.redirect("/");
+});	
+
+function isLoggedIn(req,res, next) {
+	if(req.isAuthenticated()){
+		return next();
+	}
+	res.redirect("/login")
+}
 
 app.listen(3000, function(req,res) {
 	console.log("Server started...");
