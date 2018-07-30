5c9f4a6a4fd2eefcba20d66c88dab08eb6c00b94
José Guillermo Saldaña <memo.sal98@gmail.com>
2018-07-30 17:48:14 -0500

    General passport setup

--- Exercises/Back End/Auth/app.js
+++ Exercises/Back End/Auth/app.js
@@ -1,6 +1,25 @@
-var express = require("express");
+var express = require("express"),
+		mongoose = require("mongoose"),
+		passport = require("passport"),
+		bodyParser = require("body-parser"),
+		User = require("./models/user"),
+		LocalStrategy = require("passport-local"),
+		passportLocalMongoose = require("passport-local-mongoose");
+
+mongoose.connect("mongodb://localhost:27017/auth_demo_app",{useNewUrlParser: true});
 
 var app = express();
+
+app.use(require("express-session")({
+	secret: "Rusty is the best and cutest dog in the world",
+	resave: false,
+	saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.set("view engine", "ejs");
 
