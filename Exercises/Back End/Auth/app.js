0a029435964b777ae996017478e01fa32ff506b3
José Guillermo Saldaña <memo.sal98@gmail.com>
2018-07-30 17:33:28 -0500

    Basic routes and views implemented

---
+++ Exercises/Back End/Auth/app.js
@@ -0,0 +1,17 @@
+var express = require("express");
+
+var app = express();
+
+app.set("view engine", "ejs");
+
+app.get("/", function(req,res) {
+	res.render("home");
+});
+
+app.get("/secret",function(req,res) {
+	res.render("secret");
+})
+
+app.listen(3000, function(req,res) {
+	console.log("Server started...");
+});
