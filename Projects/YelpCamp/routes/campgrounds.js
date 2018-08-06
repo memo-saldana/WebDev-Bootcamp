ab9eb67321644d98698ba8187f6d0b6ef5026cf3
José Guillermo Saldaña <memo.sal98@gmail.com>
2018-08-06 18:06:33 -0500

    Associated campgrounds to the user's account and displayed the information accordingly.

--- Projects/YelpCamp/routes/campgrounds.js
+++ Projects/YelpCamp/routes/campgrounds.js
@@ -16,23 +16,29 @@ router.get("/",function(req,res) {
 });
 
 //CREATE
-router.post("/",function(req,res) {
+router.post("/", isLoggedIn, function(req,res) {
 	var name = req.body.name;
 	var image = req.body.image;
 	var desc = req.body.description;
-	var newCamp = {name: name, image: image, description: desc};
+	var author = {
+		id: req.user._id,
+		username: req.user.username
+	}
+
+	var newCamp = {name: name, image: image, description: desc, author: author};
 	
 	Campground.create(newCamp,function(err,newlyCreated) {
 		if(err){
 			console.log(err);
 		} else {
+			console.log(newlyCreated)
 			res.redirect("/");
 		}
 	})
 });
 
 //NEW
-router.get("/new",function(req,res) {
+router.get("/new", isLoggedIn, function(req,res) {
 	res.render("campgrounds/new");
 });
 
